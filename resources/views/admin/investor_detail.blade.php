85bdfe4f527469b7540954de0029536842bc5d14
Anon Dev <michaelerastus9@gmail.com>
2024-04-18 06:52:04 +0100

    made some update in the admin

--- resources/views/admin/investor_detail.blade.php
+++ resources/views/admin/investor_detail.blade.php
@@ -216,6 +216,7 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="row">
                         <div class="col-md-12">
                             <div class=" text-center">
