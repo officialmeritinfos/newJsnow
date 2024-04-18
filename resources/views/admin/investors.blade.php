aeda4151988451a401c6069ab342ac29059bd7ec
Anon Dev <michaelerastus9@gmail.com>
2024-04-18 07:05:26 +0100

    Updated

--- resources/views/admin/investors.blade.php
+++ resources/views/admin/investors.blade.php
@@ -20,7 +20,7 @@
                         <th>Name</th>
                         <th>Username</th>
                         <th>Email</th>
-                        <th>Balance</th>
+{{--                        <th>Balance</th>--}}
                         <th>Profit</th>
                         <th>Withdrawals</th>
                         <th>Date Registered</th>
@@ -35,7 +35,7 @@
                             <td>{{$option->getInvestor($investor->id)}}</td>
                             <td>{{$investor->username}}</td>
                             <td>{{$investor->email}}</td>
-                            <td>{{number_format($investor->balance,2)}}</td>
+{{--                            <td>{{number_format($investor->balance,2)}}</td>--}}
                             <td>{{number_format($investor->profit,2)}}</td>
                             <td>{{number_format($investor->withdrawals,2)}}</td>
                             <td>{{$investor->created_at}}</td>
@@ -53,7 +53,7 @@
                                 <a href="{{route('admin.investor.detail',['id'=>$investor->id])}}" class="btn btn-primary mt-4">
                                     <i class="fa fa-eye"></i> View
                                 </a>
-                                
+
                                 <a href="{{route('admin.investor.login',['id'=>$investor->id])}}" class="btn btn-info mt-4">
                                     <i class="fa fa-eye"></i> Access User
                                 </a>
