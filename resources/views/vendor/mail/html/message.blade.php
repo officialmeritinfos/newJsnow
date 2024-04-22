5e6b753f8b6f68e4ae1f8e845bb825155b2a8bd1
Anon Dev <michaelerastus9@gmail.com>
2024-04-22 09:53:55 +0100

    Added logo

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -3,7 +3,7 @@
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
 {{--{{ config('app.name') }}--}}
-<img src="{{asset('home/images/intelligentora.png')}}" class="logo" alt="{{config('app.name')}}" style="width:100px;">
+<img src="{{asset('home/images/kinesiscrystalasset.png')}}" class="logo" alt="{{config('app.name')}}" style="width:100px;">
 @endcomponent
 @endslot
 
